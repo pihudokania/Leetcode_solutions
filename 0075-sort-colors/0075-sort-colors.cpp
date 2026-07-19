a3f3fe4dc0fe083c9d04371441e4bd15c49b1eeb
pihudokania <pihudokania19@gmail.com>
2026-07-19 11:04:07 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int count0=0;
+        int count1=0;
+        int count2=0;
+       // sort(nums.begin(),nums.end()); haha
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+            count0++;
+        }
+        if(nums[i]==1){
+         count1++;
+        }
+        if(nums[i]==2){
+            count2++;
+        }
+       }
+       for(int i=0;i<count0;i++){
+        nums[i]=0;
+       }
+       for(int i=count0;i<count0+count1;i++){
+        nums[i]=1;
+       }
+       for(int i=count0+count1;i<nums.size();i++){
+        nums[i]=2;
+       }
     }
 };
